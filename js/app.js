79e24b2237f9421b7478a47e4e68ed1aedeb852f
Shaun <scpour@gmail.com>
2023-06-03 17:23:32 -0400

    Desktop view mostly done though card still adds padding when the questions are expanded and the shadow for the desktop graphic is not present

--- js/app.js
+++ js/app.js
@@ -3,31 +3,37 @@ const arrow = document.querySelectorAll('.question-txt img');
 const answer = document.querySelectorAll('.answer-txt');
 
 question[0].addEventListener('click', () => {
+            question[0].classList.toggle('active');
             arrow[0].classList.toggle('arrow-active');
             answer[0].classList.toggle('show');
     });
 
 question[1].addEventListener('click', () => {
+            question[1].classList.toggle('active');
             arrow[1].classList.toggle('arrow-active');
             answer[1].classList.toggle('show');
     });
 
 question[2].addEventListener('click', () => {
+            question[2].classList.toggle('active');
             arrow[2].classList.toggle('arrow-active');
             answer[2].classList.toggle('show');
     });
 
 question[3].addEventListener('click', () => {
+            question[3].classList.toggle('active');
             arrow[3].classList.toggle('arrow-active');
             answer[3].classList.toggle('show');
     });
 
 question[4].addEventListener('click', () => {
+            question[4].classList.toggle('active');
             arrow[4].classList.toggle('arrow-active');
             answer[4].classList.toggle('show');
     });
 
 question[5].addEventListener('click', () => {
+            question[5].classList.toggle('active');
             arrow[5].classList.toggle('arrow-active');
             answer[5].classList.toggle('show');
     });
